6302f55b6b7d5371b6abc8345e782ed1e4e3fe82
TaigaTi <91498156+TaigaTi@users.noreply.github.com>
2022-06-01 17:11:59 -0400

    Initial commit

---
+++ Jumpy Ball/pipe.js
@@ -0,0 +1,34 @@
+class Pipe {
+    constructor(context, height) 
+    {
+        this.x = 600;
+        this.y = 0;
+        this.w = 40;
+        this.speed = 100;
+        this.spacing = Math.floor(Math.random() * (25) + (100));
+        this.top = Math.floor(Math.random() * (((3 / 4) * height) - (height / 6) + 1) + (height / 6));
+        this.bottom = height - (this.top + this.spacing);
+        this.checked = false;
+
+        // Draw Pipes
+        this.draw = function() {
+            // Draw Top Pipe
+            context.beginPath();
+            context.rect(this.x, 0, this.w, this.top);
+            context.fillRect = "white";
+            context.fill();
+
+            // Draw Bottom Pipe
+            context.beginPath();
+            context.rect(this.x, height - this.bottom, this.w, this.bottom);
+            context.fillRect = "white";
+            context.fill();
+        }
+
+        // Clear Pipe Path
+        this.clear = function() {
+            context.clearRect(this.x, 0, this.w + 5, this.top);
+            context.clearRect(this.x, height - this.bottom, this.w + 5, this.bottom);
+        }
+    }
+}
